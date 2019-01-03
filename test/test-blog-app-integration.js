ddafdc288219ad0bdf4355022cf40b86bbebcf11
Sarah Burgess <sarahburgess287@gmail.com>
2019-01-02 21:01:50 -0800

    Added test directory and test-blog-app-integration.js file, added TEST_DATABASE_URL in config.js

---
+++ test/test-blog-app-integration.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+// make expect syntax available throughout module
+const expect = chai.expect;
+
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../server.js');
+const {TEST_DATABASE_URL} = require('../config.js');
+
