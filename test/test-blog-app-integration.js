dbce15ad9154f909ecca95c9f37f09b066b28cc7
Sarah Burgess <sarahburgess287@gmail.com>
2019-01-04 19:58:07 -0800

    Added another test for the GET endpoint

--- test/test-blog-app-integration.js
+++ test/test-blog-app-integration.js
@@ -21,7 +21,7 @@ function seedBlogpostData(){
     console.info('seeding blogpost data');
     const seedData = [];
     for (let i=1; i<= 5; i++){
-        seedData.push(generateBlogpostData()); // call the function sarah
+        seedData.push(generateBlogpostData()); // call the function, sarah
     }
     // return promise
     return BlogPost.insertMany(seedData);
@@ -78,26 +78,64 @@ describe('Blogposts API resource', function() {
         return closeServer();
     })
 
-
+    // Test GET endpoint
     describe('GET endpoint', function(){
         it('should return all existing blogposts', function(){
             // strategy: get all restaurants
             // verify status and data type
             // prove number restaurants is equal to number in db
-            let blog;
+            let res;
             return chai.request(app)
                 .get('/posts')
-                .then(function(_blog) {
-                    blog = _blog;
-                    expect(blog).to.have.status(200);
+                .then(function(_res) {
+                    res = _res;
+                    expect(res).to.have.status(200);
                     //verify db of posts is more than 1
-                    expect(blog.body).to.have.lengthOf.at.least(1);
+                    expect(res.body).to.have.lengthOf.at.least(1);
                     return BlogPost.count();
                 })
                 .then(function(count) {
-                    expect(blog.body).to.have.lengthOf(count);
+                    expect(res.body).to.have.lengthOf(count);
                 });
         });
+
+        it('should return blogposts with correct fields', function() {
+            // Inspect response for correct keys
+            
+            let resBlogpost;
+            return chai.request(app)
+                .get('/posts')
+                .then(function(res) {
+                    
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.a('array');
+                    expect(res.body).to.have.lengthOf.at.least(1);
+                    console.log(res.body);
+
+                    res.body.forEach(function(post) {
+                        expect(post).to.be.a('object');
+                        expect(post).to.include.keys(
+                            'id', 'author', 'content', 'title', 'created');
+                    });
+                    // set resBlogpost to 1st blogpost in array
+                    resBlogpost = res.body[0];
+                    return BlogPost.findById(resBlogpost.id);
+                })
+                .then(function(post) {
+                    expect(resBlogpost.id).to.equal(post.id);
+                })
+
+        });
+
     });
+
+    // Test POST Endpoint
+
+
+    // Test PUT Endpoint
+
+    // Test DELETE Endpoint
+
 
 });
